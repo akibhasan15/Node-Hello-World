3b1689e46e2ed69fd22882f0e79618af2c5f15af
illusion <akibhasan15>
2023-02-22 13:10:36 +0600

    first commit

---
+++ src/app1/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello from NodeJS app1')
+})
+
+app.listen(port, () => {
+  console.log(`app1 listening on port ${port}`)
+})
